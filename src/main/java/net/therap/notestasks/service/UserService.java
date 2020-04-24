a282992eacf2541e7736de9147f9d0d60d2fa6a4
Tanmoy Krishna Das <tanmoy.das@therapservices.net>
2020-04-24 10:55:50 +0600

    Add Basic Service Layer Structure

---
+++ src/main/java/net/therap/notestasks/service/UserService.java
@@ -0,0 +1,11 @@
+package net.therap.notestasks.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author tanmoy.das
+ * @since 4/24/20
+ */
+@Service
+public class UserService {
+}
